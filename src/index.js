f239551a7d2b4a5b4a800ed48a2eadd41ab5038d
sravankumar536 <msravank97@gmail.com>
2024-08-04 17:56:05 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+   import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+   import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
